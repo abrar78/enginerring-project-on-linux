2f7d47c7008d7b6bde9a22252554eb73e04aacd0
abrar <9529693800aa@gmail.com>
2020-03-18 11:28:35 +0530

    bug fix of the input tag counter dispaly

--- advertise.js
+++ advertise.js
@@ -15,29 +15,28 @@ var lastName = document.getElementById("lastName");
 var eMail = document.getElementById("exampleInputEmail1");
 var txtArea = document.getElementById("exampleFormControlTextarea1");
 var tellSomething = document.getElementById("tellSomething");
-var chooseFile = document.getElementById("File")
-var pleaseSpecifyDiv = document.getElementById("pleaseSpecifyInputDiv")
+var chooseFile = document.getElementById("File");
+var pleaseSpecifyDiv = document.getElementById("pleaseSpecifyInputDiv");
+var specifyInput = document.getElementById("Specify");
 // *all radio inputs
 var onlyText = document.getElementById("onlyText");
 var withImage = document.getElementById("withImage");
 var other = document.getElementById("other");
 
-
 var noImage = document.getElementById("noImage");
-var noImageOpt1 = document.getElementById("optForImages")
-var noImageOpt2 = document.getElementById("opt2ForImages")
-var optLabel = document.getElementById("optLabel")
-var dontHave=document.getElementById("dontHave")
-var haveImage=document.getElementById("have")
-var haveImageInp=document.getElementById("haveImageInp")
-var dontHaveLabel=document.getElementById("dontHaveLab")
-// todo make one for you?
+var noImageOpt1 = document.getElementById("optForImages");
+var noImageOpt2 = document.getElementById("opt2ForImages");
+var optLabel = document.getElementById("optLabel");
+var dontHave = document.getElementById("dontHave");
+var haveImage = document.getElementById("have");
+var haveImageInp = document.getElementById("haveImageInp");
+var dontHaveLabel = document.getElementById("dontHaveLab");
 var yesMakeInp = document.getElementById("yes");
 var dontMakeInp = document.getElementById("no");
-
+var submit = document.getElementById("submit");
 // ? checking for the type of advertisement
 withImage.addEventListener("click", () => {
-  dontHave.checked=false;
+  dontHave.checked = false;
   if (withImage.checked == true) {
     console.log("with image is sielsected");
     chooseFile.style.display = "";
@@ -49,14 +48,12 @@ withImage.addEventListener("click", () => {
     noImageOpt2.style.display = "nome";
     optLabel.style.display = "none";
     haveImage.style.display = "none";
-
   }
 });
 
 dontHave.addEventListener("click", () => {
-  console.log("clcicked666")
+  console.log("clcicked666");
 
- 
   if (dontHave.checked == true) {
     chooseFile.style.display = "none";
     noImageOpt1.style.display = "";
@@ -69,16 +66,16 @@ dontHave.addEventListener("click", () => {
     optLabel.style.display = "none";
     chooseFile.style.display = "";
   }
-})
-haveImageInp.addEventListener("click", ()=>{
-  if (haveImageInp.checked==true) {
+});
+haveImageInp.addEventListener("click", () => {
+  if (haveImageInp.checked == true) {
     chooseFile.style.display = "";
     haveImage.style.display = "none";
     noImageOpt1.style.display = "none";
     noImageOpt2.style.display = "none";
-    optLabel.style.display = "none";  
+    optLabel.style.display = "none";
   }
-})
+});
 other.addEventListener("click", () => {
   if (other.checked == true) {
     console.log("other is sielsected");
@@ -89,7 +86,7 @@ other.addEventListener("click", () => {
     dontHaveLabel.style.display = "none";
     noImageOpt1.style.display = "none";
     noImageOpt2.style.display = "none";
-    optLabel.style.display = "none";  
+    optLabel.style.display = "none";
   }
 });
 onlyText.addEventListener("click", () => {
@@ -122,6 +119,223 @@ if (other.checked == true) {
 
 // todo>>>>> regular expression form validation start from here
 
-var firstNameRegExp=/[a-zA-Z]{3,10}/g
-var lastNameRegExp=/[a-zA-Z]{3,10}/g
-var emailRegExp=/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/gi
+var firstNameRegExp = new RegExp("[a-zA-Z]{3,20}", "g");
+var lastNameRegExp = new RegExp("[a-zA-Z]{3,20}", "g");
+var emailRegExp = new RegExp(
+  "(^[A-Z0-9._%+-]+)(@[A-Z0-9.-]+)(.[A-Z]{2,})",
+  "gi"
+);
+// new RegExp("[a-zA-Z0-9]{30,}", "g");
+var infoOfAdvertRegEx = /[a-zA-Z0-9\s]{50,200}$/gy;
+var specificationOfAdvertRegEx = new RegExp("[a-zA-Z0-9s]{30,100}$", "gy");
+// !firstName lastName eMail txtArea(specify) tellSomething
+var resultFirstName = false;
+var resultLastName = false;
+var resultEMail = false;
+var resultSpecify = false;
+var resultTellSomething = false;
+var resultFile = false;
+
+firstName.addEventListener("focus", () => {
+  resultFirstName = false;
+  firstName.value = "";
+  firstName.classList.remove("successInput");
+  firstName.classList.remove("wrongInput");
+});
+firstName.addEventListener("blur", e => {
+  firstNameRegExp.lastIndex = 0;
+
+  resultFirstName = firstNameRegExp.test(firstName.value);
+
+  if (resultFirstName == true) {
+    firstName.classList.add("successInput");
+    firstName.classList.remove("wrongInput");
+  }
+  if (resultFirstName == false) {
+    firstName.classList.add("wrongInput");
+    firstName.classList.remove("successInput");
+  }
+});
+
+lastName.addEventListener("focus", () => {
+  resultLastName = false;
+  lastName.value = "";
+  lastName.classList.remove("successInput");
+  lastName.classList.remove("wrongInput");
+});
+lastName.addEventListener("blur", () => {
+  resultLastName = lastNameRegExp.test(lastName.value);
+
+  if (resultLastName == true) {
+    lastName.classList.add("successInput");
+    lastName.classList.remove("wrongInput");
+  }
+  if (resultLastName == false) {
+    lastName.classList.add("wrongInput");
+    lastName.classList.remove("successInput");
+  }
+});
+
+eMail.addEventListener("focus", () => {
+  eMail.value = "";
+
+  resultEMail = false;
+  eMail.classList.remove("successInput");
+
+  eMail.classList.remove("wrongInput");
+});
+eMail.addEventListener("blur", e => {
+  emailRegExp.lastIndex = 0;
+
+  resultEMail = emailRegExp.test(eMail.value);
+
+  if (resultEMail == true) {
+    eMail.classList.add("successInput");
+    eMail.classList.remove("wrongInput");
+  }
+  if (resultEMail == false) {
+    eMail.classList.add("wrongInput");
+    eMail.classList.remove("successInput");
+  }
+});
+var charCounterTellSomething = 0;
+var counterDisplayTellSomething = document.getElementById("chrCountTellSomething")
+var key1;
+tellSomething.addEventListener("blur", e => {
+  resultTellSomething = false;
+  resultTellSomething = infoOfAdvertRegEx.test(tellSomething.value);
+
+  if (resultTellSomething == true) {
+    tellSomething.classList.add("successInput");
+    tellSomething.classList.remove("wrongInput");
+  }
+  if (resultTellSomething == false) {
+    tellSomething.classList.add("wrongInput");
+    tellSomething.classList.remove("successInput");
+  }
+});
+
+tellSomething.addEventListener("input", function() {
+  tellSomething.onkeydown = function() {
+    key1 = event.keyCode || event.charCode;
+    if (key1 == 13) {
+      return false;
+    }
+    console.log("key1:", key1);
+  };
+  if (tellSomething.value == "") {
+    charCounterTellSomething = 1;
+  }
+  if (key1 == 8) {
+    charCounterTellSomething--;
+    console.log(charCounterTellSomething);
+  } else {
+    charCounterTellSomething++;
+    console.log(charCounterTellSomething);
+  }
+  counterDisplayTellSomething.innerText = '50 to 200 characters are allowed' +" "+"["+charCounterTellSomething+"]";
+});
+// !-----------------------------------------------------------------------------------------------------------------
+
+var charCounterSpecify = 0;
+var counterDisplaySpecify = document.getElementById("chrCountSpecify");
+var key2;
+specifyInput.addEventListener("focus", () => {
+  specifyInput.onkeydown = function() {
+    key2 = event.keyCode || event.charCode;
+    if (key2 == 13) {
+      return false;
+    }
+    console.log("key:", key2);
+  };
+  infoOfAdvertRegEx.lastIndex = 0;
+  resultSpecify = false;
+});
+specifyInput.addEventListener("blur", e => {
+  resultSpecify = false;
+  resultSpecify = infoOfAdvertRegEx.test(specifyInput.value);
+
+  if (resultSpecify == true) {
+    specifyInput.classList.add("successInput");
+    specifyInput.classList.remove("wrongInput");
+  }
+  if (resultSpecify == false) {
+    specifyInput.classList.add("wrongInput");
+    specifyInput.classList.remove("successInput");
+  }
+});
+
+specifyInput.addEventListener("input", function() {
+  specifyInput.onkeydown = function() {
+    key2 = event.keyCode || event.charCode;
+    if (key2 == 13) {
+      return false;
+    }
+    console.log("key:", key2);
+  };
+  if (specifyInput.value == "") {
+    charCounterSpecify = 1;
+  }
+  if (key2 == 8) {
+    charCounterSpecify--;
+    console.log(charCounterSpecify);
+  } else {
+    charCounterSpecify++;
+    console.log(charCounterSpecify);
+  }
+  counterDisplaySpecify.innerText = ` 50 to 200 characters are allowed [${charCounterSpecify}]`;
+});
+
+var fileInput=document.getElementById("XFile")
+var tooLarge=document.getElementById("tooLarge")
+fileInput.onchange=function(){
+if(fileInput.files[0].size<=10485760){
+  resultFile=true;
+  tooLarge.style.display="none";
+}
+else{
+  resultFile=false;
+  tooLarge.style.display="";
+}
+console.log(fileInput.files[0].size);
+}
+// var resultFirstName = false;
+// var resultLastName = false;
+// var resultEMail = false;
+// var resultSpecify = false;
+// var resultTellSomething = false;
+// var resultFile = false;
+function validateForm() {
+  var result = true;
+  if (resultFirstName==false) {
+    result=false;
+  }
+  if (resultLastName==false) {
+    result=false;
+  }
+  if (resultEmailtName==false) {
+    result=false;
+  }
+  if (resultTellSomething==false) {
+    result=false;
+  }
+  if(withImage.checked){
+           if(resultFile==false){
+             result=false;
+           }
+
+  }
+  if(other.checked){
+           if(resultSpecify==false){
+             result=false;
+           }
+  }
+  console.log("firstName",resultFirstName)
+  console.log("lastName",resultLastName)
+  console.log("email",resultEMail)
+  console.log("tellsomething",resultTellSomething)
+  console.log("image",resultImage)
+  console.log("specify",resultSpecify)
+  console.log("total",result)
+  return result;
+}

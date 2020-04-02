b0e367dcaf396067432f257824581dcd51051f41
abrar <9529693800aa@gmail.com>
2020-04-02 10:47:06 +0530

    flask has been added to the html files

--- static/animate.js
+++ static/animate.js
@@ -1,4 +1,6 @@
 console.log("this ios custom for read more jS");
+var w;
+var h;
 function myFunction() {
     var w = window.outerWidth;
     var h = window.outerHeight;
@@ -16,13 +18,18 @@ function howerLeave(i) {
 }
 var nav = document.getElementById("navBar")
 console.log(nav)
+var bottom=document.getElementById("bottom")
 window.onscroll = function () {
-    if (window.pageYOffset > 500) {
+    
+    if (window.pageYOffset> 500) {
+        console.log(window.pageXOffset);
         nav.style.background = "rgb(0, 0, 0,.8)"
     } else {
         nav.style.background = "rgb(1, 41, 65,0)"
     }
-}
+   
+    }
+
 
 // Type writing effet 
 const text = ["Sign Up  for latest updates", "Blog for Engineers", "Blog for Electronic-Hobyist"]
@@ -179,6 +186,49 @@ function customPage(tempId){
     requireDiv.classList.remove(removeAnim);
     requireDiv.classList.add(animation);
 
+}
 
+// *responsive Java Script
+var search=document.getElementById("search")
+var mobileSearchBar=document.getElementById("mobileSearchBar");
+var mobileNav=document.getElementById("mobileNav")
+var closeSearch=document.getElementById("closeSearch");
+var lock=true;
+search.onclick= function(){
+    console.log("seacrh clicked");
+    mobileSearchBar.style.display="flex";
+    mobileNav.style.display="none";
+    lock=false;
 
+}
+
+var burger=document.getElementById("burger")
+var slider=document.getElementById("slider")
+var closeSlider=document.getElementById("closeSlider")
+var lockSlider=true
+burger.onclick=function(){
+    slider.style.display="flex";
+    slider.classList.add("slideInLeft")
+    slider.classList.remove("slideOutLeft")
+    // slider.classList.remove="slideOutLeft"
+    lockSlider=false;
+}
+closeSearch.onclick=function(){
+    console.log("clicked parallex function");
+    if(lock==false) {
+        
+        mobileSearchBar.style.display="none";
+        mobileNav.style.display="flex";
+        lock=true;
+    }
+  if (lockSlider==false) {
+      slider.style.display="none";
+      lockSlider=true;
+  }    
+}
+closeSlider.onclick=function(){
+    console.log("clicked close");
+    slider.classList.remove("slideInLeft")
+    slider.classList.add("slideOutLeft")
+    
 }
